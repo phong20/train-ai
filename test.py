246fa4089422952b734c00808a7f1745fa72699b
phong20 <98086983+phong20@users.noreply.github.com>
2024-08-07 17:34:33 +0700

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from transformers import GPT2Tokenizer, GPT2LMHeadModel
 model_name = 'gpt2-xl'
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 model = GPT2LMHeadModel.from_pretrained(model_name)
-input_text = "Hãy viết một đoạn văn bản tiếp theo từ đây:"
+input_text = "dịch đoạn văn này sang tiếng việt: 寿命を買い取ってもらえるという話を聞いたとき、俺が真っ先に思い出したのは、小学生の頃に受けた道徳の授業のことだった。まだ自分でものを考えるということを知らない十歳の俺たちに向けて、学級担任である二十代後半の女性教員は、こんなふうに問いかけた。"
 input_ids = tokenizer.encode(input_text, return_tensors='pt')
 output = model.generate(input_ids, max_length=100, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
 generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
